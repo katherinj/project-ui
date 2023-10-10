b2ae9dd2167ea030b1937a4227d8bc4090da2d11
Katherin Jimenez <katjimenez98@gmail.com>
2023-10-10 17:10:53 -0400

    Updating apiClient file

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -34,19 +34,51 @@ class ApiClient {
   // ----------------------- movie requests -----------------------------
 
   // returns all movies
+  // GET /movie
   async fetchAllMovies() {
     return await this.request({ endpoint: `movie`, method: `GET` });
   }
 
-  // returns top 5 movies
+  // returns top 5 movies and its details
+  // GET /movie/top5
   async fetchTopFiveMovies() {
     return await this.request({ endpoint: `movie/top5`, method: `GET` });
   }
 
+  // returns movie and movie details by ID
   // GET /movie/:movieId
   async fetchMovieById(movieId) {
     return await this.request({ endpoint: `movie/${movieId}`, method: `GET` });
   }
+
+  // returns the top 5 actors for a movie
+  // GET /movie/top5actors/:movieId
+  async fetchMoviesTop5Actors(movieId) {
+    return await this.request({
+      endpoint: `movie/top5actors/${movieId}`,
+      method: `GET`,
+    });
+  }
+
+  // ----------------------- actor requests -----------------------------
+  // returns all actors and their details
+  // GET /actor
+
+  // returns top 5 actors for a movie and their details
+  // GET actor/top5/:movieId
+
+  // ----------------------- customer requests -----------------------------
+  // returns all customers and their details
+  // GET /customer
+
+  // add a new customer
+  // POST /customer
+
+  // edit a specific customers details using their ID
+  // UPDATE /customer/:customerId
+
+  // returns a customer and their details by ID
+  // GET /customer/:customerId
 }
 
 // export default new ApiClient(API_BASE_URL || "http://localhost:3001")
